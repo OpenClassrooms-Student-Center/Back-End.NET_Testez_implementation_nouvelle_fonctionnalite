6d2daca43f4bd87e935a86e917b2d1a904fe33b4
NXTMax <maxenceP25@gmail.com>
2024-04-05 13:59:38 +0200

    Add tests for product validation

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,154 @@
-﻿using Xunit;
+﻿using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models;
+using Xunit;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        public class CheckProductModelErrors
         {
-            // Arrange
+            private readonly Mock<ICart> mockCart;
+            private readonly Mock<IProductRepository> mockProductRepository;
+            private readonly Mock<IOrderRepository> mockOrderRepository;
+            private readonly Mock<IStringLocalizer<ProductService>> mockLocalizer;
 
-            // Act
+            public CheckProductModelErrors()
+            {
+                // Setup
+                mockCart = new Mock<ICart>();
+                mockProductRepository = new Mock<IProductRepository>();
+                mockOrderRepository = new Mock<IOrderRepository>();
+                mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
+                foreach (var property in typeof(Resources.ProductService).GetProperties())
+                {
+                    if (property.Name == "ResourceManager" || property.Name == "Culture" || property.Name == "ResourceCulture")
+                        continue;
+                    mockLocalizer.Setup(l => l[property.Name]).Returns(new LocalizedString(property.Name, property.GetValue(null).ToString()));
+                }
+            }
 
+            [Fact]
+            public void ShouldSucceed_WhenProductIsWellFormed()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
 
-            // Assert
-            Assert.Equal(1, 1);
+                var product = new ProductViewModel()
+                {
+                    Name = "Test",
+                    Stock = "1",
+                    Price = "1,5"
+                };
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Empty(modelErrors);
+            }
+
+            [Fact]
+            public void ShouldAddModelErrors_WhenProductIsEmpty()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
+
+                var product = new ProductViewModel();
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Contains(Resources.ProductService.MissingName, modelErrors);
+                Assert.Contains(Resources.ProductService.MissingPrice, modelErrors);
+                Assert.Contains(Resources.ProductService.MissingStock, modelErrors);
+            }
+
+            [Fact]
+            public void ShouldAddModelErrors_WhenPriceIsNotANumber()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
+
+                var product = new ProductViewModel()
+                {
+                    Name = "Test",
+                    Stock = "1",
+                    Price = "NotADouble"
+                };
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Contains(Resources.ProductService.PriceNotANumber, modelErrors);
+            }
+
+            [Fact]
+            public void ShouldAddModelErrors_WhenPriceIsNotGreaterThanZero()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
+
+                var product = new ProductViewModel()
+                {
+                    Name = "Test",
+                    Stock = "1",
+                    Price = "0"
+                };
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Contains(Resources.ProductService.PriceNotGreaterThanZero, modelErrors);
+            }
+
+            [Fact]
+            public void ShouldAddModelErrors_WhenStockIsNotAnInteger()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
+
+                var product = new ProductViewModel()
+                {
+                    Name = "Test",
+                    Stock = "NotAnInteger",
+                    Price = "1"
+                };
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Contains(Resources.ProductService.StockNotAnInteger, modelErrors);
+            }
+
+            [Fact]
+            public void ShouldAddModelErrors_WhenStockIsNotGreaterThanZero()
+            {
+                // Arrange
+                var service = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockLocalizer.Object);
+
+                var product = new ProductViewModel()
+                {
+                    Name = "Test",
+                    Stock = "0",
+                    Price = "1"
+                };
+
+                // Act
+                List<string> modelErrors = service.CheckProductModelErrors(product);
+
+                // Assert
+                Assert.Contains(Resources.ProductService.StockNotGreaterThanZero, modelErrors);
+            }
         }
-
-        // TODO write test methods to ensure a correct coverage of all possibilities
     }
 }
